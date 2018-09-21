49c9402f85fabe8310677594d5768e942f5e8474
tatia-kom <komarovats93@gmail.com>
2018-09-21 15:53:31 +0300

    Верстка

--- js/scripts.js
+++ js/scripts.js
@@ -38,13 +38,27 @@ $(document).ready(function () {
     });
     
     $('.type-car').change(function() {
-        if($('.checkbox-3').is(":checked")) {
-          
+        if($('#checkbox-3').prop('checked')) {
+            if($('#checkbox-4').prop('checked')) {
+                $('.flex-form__side--submit .image').removeClass('image--bus').addClass('image--both');
+            }
+            else {
+                $('.flex-form__side--submit .image').removeClass('image--both').removeClass('image--bus');
+            }
         }
-        if($('.checkbox-4').is(":checked")) {
-          
+        else if ($('#checkbox-4').prop('checked')) {
+            $('.flex-form__side--submit .image').removeClass('image--both').addClass('image--bus');
         }
         
+    });
+
+    $('.popular__flex__item__head').mouseover(function () {
+        $(this).find('.popular__flex__item__price_block__vis').addClass('hid');
+        $(this).find('.popular__flex__item__price_block__hid').addClass('show');
+    });
+    $('.popular__flex__item__head').mouseleave(function () {
+        $(this).find('.popular__flex__item__price_block__hid').removeClass('show');
+        $(this).find('.popular__flex__item__price_block__vis').removeClass('hid');
     });
 });
     
